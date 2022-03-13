4a841d724a2855e05e6ded53b9aaefed0c8d3355
Dongwoo <kimphysicsman@gmail.com>
2022-03-13 18:10:43 +0900

    like commit

--- static/js/main.js
+++ static/js/main.js
@@ -28,22 +28,6 @@ function closePopup() {
 }
 popup.addEventListener('click', closePopup);
 
-// 좋아요 버튼 클릭 시 하트 변경
-// function toggleLike() {
-  // document.getElementById("likebtn").src = "../static/images/like_icon_hover.png";
-// }
-let likecnt = 1;
-function toggleLike() {
-  let like = document.getElementById("likebtn");
-  
-  if(likecnt % 2 == 1) {
-    like.src = '../static/images/like_icon_hover.png';
-  } else {
-    like.src = '../static/images/like_icon.png';
-  }
-  likecnt++;
-}
-
 // RetroChart 업데이트
 function retroChart(chart_year) {
   $.ajax({
@@ -110,23 +94,29 @@ function main_playing_active(songID) {
                 data: { songID_give: songID
                 },
                 success: function (response) {
-                    singer = response['music_info']['singer']
-                    title = response['music_info']['title']
-                    musicPlaySrc = response['music_info']['musicPlaySrc']
+                    let userID = response['music_info']['id']
+                    let singer = response['music_info']['singer']
+                    let title = response['music_info']['title']
+                    let musicPlaySrc = response['music_info']['musicPlaySrc']
+                    let like = response['music_info']['like']
 
-                    console.log(singer, title, musicPlaySrc)
                     let temp_html = `<div class="youtube_movie">
                                         <iframe width="100" height="75" src="${musicPlaySrc}?enablejsapi=1&version=3&playerapiid=ytplayer&autoplay=1&mute=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                      </div>
                                      <div class="playbar_song_wrap">
                                         <div class="playbar_song_title">${title}</div>
                                         <div class="playbar_song_artist">${singer}</div>
-                                     </div>
-                                     <img src="../static/images/like_icon.png" alt="" id="likebtn" onclick="toggleLike()">`
-
+                                     </div>`
+                    let temp_html2 = ``
+                    if (like == 0) {
+                        temp_html2 = `<img src="../static/images/like_icon.png" alt="" id="likebtn" onclick="likeclick('${userID}', '${title}', '${singer}')">`
+                    } else {
+                        temp_html2 = `<img src="../static/images/like_icon_hover.png" alt="" id="likebtn" onclick="likeclick('${userID}', '${title}', '${singer}')">`
+                    }
 
                     $('#playbar_song').empty()
                     $('#playbar_song').append(temp_html)
+                    $('#playbar_song').append(temp_html2)
 
                     temp_html = `<td id="player_active" style="display:none">1</td>`
                     $('#playbar_control').empty();
