46e483ee3b7da07ca67ed7f21e6bb554fbcf8054
Dongwoo <kimphysicsman@gmail.com>
2022-03-15 00:41:36 +0900

    search commit2

--- static/js/search.js
+++ static/js/search.js
@@ -4,3 +4,27 @@ function enterkey() {
        console.log("엔터키 입력");
   }
 }
+
+search_loading('사랑')
+
+
+// 검색 결과 페이지
+function search_loading(word) {
+  $.ajax({
+    type: 'POST',
+    url: '/search',
+    data: {
+        word_give: word
+    },
+    success: function (response) {
+        // response가 어떤 정보를 가지고있는지 확인하시고 코드 작성하시면 됩니다.
+        console.log(response)
+
+
+
+
+
+        alert(response['msg'])
+    }
+  });
+}
